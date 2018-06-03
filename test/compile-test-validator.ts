df528570c7203f43f7aca612f41ab0e71e1dbc6a
Christoph Hoffmann <Christoph.Hoffmann@adesso.de>
2018-06-03 20:24:28 +0200

    tests for TypedAsyncValidatorFn

--- test/compile-test-validator.ts
+++ test/compile-test-validator.ts
@@ -1,4 +1,4 @@
-import {AbstractControl, AbstractControlOptions, FormControl} from '../model';
+import {AbstractControl, AsyncValidatorFn, FormControl, TypedAsyncValidatorFn} from '../model';
 import {AbstractControl as NgAbstractControl} from '@angular/forms';
 import {Hero} from './interfaces';
 
@@ -15,8 +15,28 @@ new FormControl<number>(42, [(c: AbstractControl<number>) => null, (c: NgAbstrac
 
 // partial TypedValidatorFn
 new FormControl<Hero>(undefined, (c: AbstractControl<{ power: string }>) => null);
-new FormControl<Hero>(undefined, [(c: AbstractControl<{ power: string }>) => null, (c: AbstractControl<{ sidekick: string }>) => null]);
+new FormControl<Hero>(undefined, [(c: AbstractControl<{ power: string }>) => null,
+  (c: AbstractControl<{ sidekick: string }>) => null]);
 
 // using AbstractControlOptions
 new FormControl<Hero>(undefined, {validators: (c: AbstractControl<{ power: string }>) => null});
 new FormControl<Hero>(undefined, {validators: [(c: AbstractControl<{ power: string }>) => null]});
+
+// TypedAsyncValidatorFn
+new FormControl<number>(42, null, (c: AbstractControl<number>) => Promise.resolve(null));
+// TypedAsyncValidatorFn[]
+new FormControl<number>(42, null, [(c: AbstractControl<number>) => Promise.resolve(null)]);
+// AsyncValidatorFn
+new FormControl<number>(42, null, (c: NgAbstractControl) => Promise.resolve(null));
+// AsyncValidatorFn[]
+new FormControl<number>(42, null, [(c: NgAbstractControl) => Promise.resolve(null)]);
+// mixed AsyncValidatorFn<T>
+new FormControl<number>(42, null, [(c: AbstractControl<number>) => Promise.resolve(null),
+  (c: NgAbstractControl) => Promise.resolve(null)]);
+
+// partial AsyncTypedValidatorFn
+new FormControl<Hero>(undefined, null, (c: AbstractControl<{ power: string }>) => Promise.resolve(null));
+new FormControl<Hero>(undefined, null,
+  [(c: AbstractControl<{ power: string }>) => Promise.resolve(null),
+    (c: AbstractControl<{ sidekick: string }>) => Promise.resolve(null)]);
+
