8f381d84d9b1bdb73755fb57e93c279364fc0814
Rohan Hart <rohan.hart@edpay.nz>
2019-10-22 11:09:59 +1300

    infer control types
    better inference for 'get'
    to infer the types correctly the model type must not be unknown

--- src/interop.ts
+++ src/interop.ts
@@ -4,12 +4,12 @@ import {
   FormControl as NgFormControl,
   FormGroup as NgFormGroup
 } from '@angular/forms';
-import {AbstractControl, FormArray, FormControl, FormGroup} from './model';
+import { AbstractControl, FormArray, FormControl, FormGroup, Controls} from './model';
 
 export function toUntyped<T>(control: FormControl<T>): NgFormControl;
-export function toUntyped<T>(control: FormGroup<T>): NgFormGroup;
-export function toUntyped<T>(control: FormArray<T>): NgFormArray;
-export function toUntyped<T>(control: AbstractControl<T>): NgAbstractControl;
+export function toUntyped<T>(control: FormGroup<Controls<any>>): NgFormGroup;
+export function toUntyped<T>(control: FormArray<AbstractControl<any>>): NgFormArray;
+export function toUntyped<T>(control: AbstractControl<any, any>): NgAbstractControl;
 export function toUntyped<T>(control: any): any {
   return control as any;
 }
--- test/compile-test-form-builder.ts
+++ test/compile-test-form-builder.ts
@@ -1,6 +1,6 @@
 import {Validators} from '@angular/forms'
-import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup} from '../src/model';
-import {Address, Bar, Foo, Hero} from './interfaces';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Controls} from '../src/model';
+import {Address, Bar, Hero} from './interfaces';
 
 const fb = new FormBuilder();
 // FormControl
@@ -9,29 +9,31 @@ let stringFormControl: FormControl<string>;
 
 stringFormControl = fb.control('');
 stringFormControl = fb.control({value: '', disabled: true});
-// FormArray
-let stringFormArray: FormArray<string>;
 
-stringFormArray = fb.array<string>(['2345', 'ieiae', fb.control<string>('uieie')]);
-stringFormArray = fb.array<string>([fb.control<string>('uieie')]);
+// FormArray
+let stringFormArray: FormArray<FormControl<string>>;
+
+const stringFormArrayGeneric: FormArray<AbstractControl<string>> = fb.array<string>(['2345', 'ieiae', fb.control<string>('uieie')]);
+stringFormArray = fb.array([fb.control<string>('uieie')]);
 stringFormArray.setValue(['foo', 'bar']);
-let barFormArray: FormArray<Bar>;
+
+let barFormArray: FormArray<AbstractControl<Bar>>;
 barFormArray = fb.array([fb.control({prop: ''})]);
 barFormArray = fb.array<Bar>([[{prop: ''}, Validators.required]]);
 barFormArray = fb.array<Bar>([[{prop: ''}, [Validators.required]]]);
 barFormArray = fb.array<Bar>([[{prop: ''}, [Validators.required, Validators.minLength(5)]]]);
 
 // FormGroup
-const heroFormGroup: FormGroup<Hero> = fb.group<Hero>({
+const heroFormGroup = fb.group<Hero>({
   name: '',
   secretLairs: fb.array<Address>([new Address(), {name: 'foo'}]),
   power: '',
   sidekick: ''
 });
-const namecontrol: AbstractControl<string> | null = heroFormGroup.get('name');
-const lairs: AbstractControl<Address[]> | null = heroFormGroup.get('secretLairs');
+const namecontrol: AbstractControl<string> = heroFormGroup.get('name');
+const lairs: AbstractControl<Address[]> = heroFormGroup.get('secretLairs');
 
-let barFormGroup: FormGroup<Bar>;
+let barFormGroup: FormGroup<Controls<Bar>>;
 barFormGroup= fb.group<Bar>({prop: ''});
 barFormGroup = fb.group<Bar>({prop: {value: '', disabled: true}});
 barFormGroup = fb.group<Bar>({prop: ['', Validators.required]});
