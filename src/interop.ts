5f05b5ab750f9ad4db01bc97b9c03cef89497769
Rohan Hart <rohan.hart@edpay.nz>
2019-11-07 11:38:27 +1300

    toUntyped needs to be more generic

--- src/interop.ts
+++ src/interop.ts
@@ -7,8 +7,8 @@ import {
 import { AbstractControl, FormArray, FormControl, FormGroup, Controls} from './model';
 
 export function toUntyped<T>(control: FormControl<T>): NgFormControl;
-export function toUntyped<T>(control: FormGroup<Controls<any>>): NgFormGroup;
-export function toUntyped<T>(control: FormArray<AbstractControl<any>>): NgFormArray;
+export function toUntyped<T extends Controls<any>>(control: FormGroup<T>): NgFormGroup;
+export function toUntyped<T extends AbstractControl<any>>(control: FormArray<T>): NgFormArray;
 export function toUntyped<T>(control: AbstractControl<any, any>): NgAbstractControl;
 export function toUntyped<T>(control: any): any {
   return control as any;
